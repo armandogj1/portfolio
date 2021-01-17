1aaa08454f61d0076126af25ae7777ab1af3d898
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2021-01-17 17:51:09 -0500

    Refactor to use getStaticPaths and getStaticProps

--- pages/projects/[project].jsx
+++ pages/projects/[project].jsx
@@ -1,11 +1,34 @@
 import { useRouter } from 'next/router';
 import Layout from '../../src/Layout';
+import projectsData from '../../src/assets/projectData.json';
 
-const Project = () => {
-	const router = useRouter();
-	const { project } = router.query;
+const Project = ({ name, github }) => {
+  const router = useRouter();
+  const { project } = router.query;
 
-	return <h1>{`This ${project}`}</h1>;
+  return (
+    <>
+      <h1>{`This ${name}`}</h1>
+      <a href={github}>🐙🐱</a>
+    </>
+  );
 };
 
 export default Project;
+
+export const getStaticPaths = async () => {
+  const paths = projectsData.map((project) => ({
+    params: { project: project.path },
+  }));
+
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const [project] = projectsData.filter((p) => params.project === p.path);
+
+  console.log(project);
+  return {
+    props: { ...project },
+  };
+};
--- pages/projects/index.jsx
+++ pages/projects/index.jsx
@@ -1,25 +1,29 @@
 import Link from 'next/link';
 import Layout from '../../src/Layout';
 import ProjectTile from '../../src/ProjectTile';
-import projects from '../../src/assets/projectData.json';
+import projectsData from '../../src/assets/projectData.json';
 
-const Projects = ({ children = ['Hello'] }) => {
-	return (
-		<div id='main-block-projects'>
-			<h3>SOME PROJECTS I'VE WORKED ON</h3>
-			{projects.map((project, idx) => (
-				<Link key={`${project.name} ${idx}`} href='/projects/apptendance'>
-					<a>
-						<ProjectTile
-							project={project.name}
-							image={project.image}
-							description={project.description}
-						/>
-					</a>
-				</Link>
-			))}
-		</div>
-	);
+const Projects = ({ children = ['Hello'], projects }) => {
+  return (
+    <div id='main-block-projects'>
+      <h3>SOME PROJECTS I'VE WORKED ON</h3>
+      {projects.map((project, idx) => (
+        <Link key={`${project.name} ${idx}`} href={`/projects/${project.path}`}>
+          <a>
+            <ProjectTile {...project} />
+          </a>
+        </Link>
+      ))}
+    </div>
+  );
+};
+
+export const getStaticProps = async () => {
+  return {
+    props: {
+      projects: projectsData,
+    },
+  };
 };
 
 export default Projects;
