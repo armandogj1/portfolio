51e221858be568137371e76c7558bd78b8f95a54
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2021-01-17 17:49:34 -0500

    Make modifications

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,26 +2,26 @@ import Head from 'next/head';
 import Menu from './Menu';
 
 const Layout = ({ children, title }) => {
-	const buttons = [
-		{ path: '/', label: 'Home' },
-		{ path: '/projects', label: 'Projects' },
-		{ path: '/resume', label: 'Resume' },
-	];
+  const buttons = [
+    { path: '/', label: 'Home' },
+    { path: '/projects', label: 'Projects' },
+    { path: '/resume', label: 'Resume' },
+  ];
 
-	return (
-		<div id='layout'>
-			<Head>
-				<title>portfolio</title>
-				<meta charSet='utf-8' />
-				<meta name='viewport' content='initial-scale=1.0, width=device-width' />
-			</Head>
-			<header id='header'>
-				<Menu buttons={buttons}></Menu>
-			</header>
-			<div id='main-block'>{children}</div>
-			<footer>{'I`m here to stay'}</footer>
-		</div>
-	);
+  return (
+    <div id='layout'>
+      <Head>
+        <title>portfolio</title>
+        <meta charSet='utf-8' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
+      <header id='header'>
+        <Menu buttons={buttons}></Menu>
+      </header>
+      <div id='main-block'>{children}</div>
+    </div>
+  );
 };
 
+// this
 export default Layout;
--- src/ProjectTile.jsx
+++ src/ProjectTile.jsx
@@ -1,22 +1,22 @@
 import Image from 'next/image';
 
-const ProjectTile = ({ project, image, description }) => {
-	return (
-		<div className='project-tile'>
-			<div className='project-tile-text'>
-				<h3>{project}</h3>
-				<p>{description}</p>
-			</div>
-			<div className='project-tile-overlay'>
-				<Image
-					src={`/${image}`}
-					alt={`${project} Image`}
-					width='200'
-					height='200'
-				/>
-			</div>
-		</div>
-	);
+const ProjectTile = ({ name, image, description, github }) => {
+  return (
+    <div className='project-tile'>
+      <div className='project-tile-text'>
+        <h3>{name}</h3>
+        <p>{description}</p>
+      </div>
+      <div className='project-tile-overlay'>
+        <Image
+          src={`/${image}`}
+          alt={`${name} Image`}
+          width='200'
+          height='200'
+        />
+      </div>
+    </div>
+  );
 };
 
 export default ProjectTile;
