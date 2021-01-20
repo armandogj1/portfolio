a72a9966fdb8b0445095715907e66b2659b17743
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2021-01-20 11:13:43 -0500

    Include resume css module

--- pages/resume.jsx
+++ pages/resume.jsx
@@ -1,8 +1,10 @@
 import Layout from '../src/Layout';
+import styles from '../src/styles/resume.module.css';
 
 const Resume = () => {
   return (
     <embed
+      className={styles.resume}
       type='application/pdf'
       src='./cv.pdf'
       height='100%'
