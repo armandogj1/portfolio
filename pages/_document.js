76a314de1c7fcb6c81f366667d30b6a16d8dcbc2
Jorge Armando Garcia-Jacquier <j.armando.garcia.jacquier@gmail.com>
2021-01-19 22:15:55 -0500

    Modify media queries, Include more fonts, Change hover effect

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,23 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
-	render() {
-		return (
-			<Html>
-				<Head>
-					<link rel='preconnect' href='https://fonts.gstatic.com' />
-					<link
-						href='https://fonts.googleapis.com/css2?family=Kameron&family=Roboto:wght@100;700&display=swap'
-						rel='stylesheet'
-					/>
-				</Head>
-				<body>
-					<Main />
-					<NextScript />
-				</body>
-			</Html>
-		);
-	}
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link
+            href='https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;700&display=swap'
+            rel='stylesheet'
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
 
 export default MyDocument;
--- pages/index.jsx
+++ pages/index.jsx
@@ -5,12 +5,14 @@ import styles from '../src/styles/index.module.css';
 
 const App = (props) => {
   return (
-    <section className={styles.intro}>
-      <p>Hello, my name is</p>
-      <h1>Armando Garcia-Jacquier</h1>
+    <section className={styles.index}>
+      <div className={styles.intro}>
+        <p>Hello, my name is</p>
+        <h1>Armando Garcia-Jacquier</h1>
+      </div>
       <p>
         I am a full-stack developer, living in NYC focusing on making
-        intereseting things while keeping it simple and beautiful.
+        intereseting things while keeping it simple.
       </p>
       <Link href='/projects'>
         <a id='main-page-button'>Checkout My Work</a>
